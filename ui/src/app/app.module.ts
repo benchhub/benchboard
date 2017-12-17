76953cc06c01415557c0400e7bdeeb578d8da908
at15 <at15@outlook.com>
2017-12-17 02:07:29 -0800

    [ui] Use ngx-echarts Fix #1
    - Angular 5 + NG-ZORRO + ngx-echarts
    - pretty heavy weight for such a simple task

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { NgxEchartsModule } from 'ngx-echarts';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -15,6 +16,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     NgZorroAntdModule.forRoot(),
+    NgxEchartsModule,
     AppRoutingModule
   ],
   providers: [],
