e0efe4aa00b7d7e0a18f738c8bb41312a849e321
at15 <at15@outlook.com>
2017-12-17 01:45:36 -0800

    [ui] Add NG-ZORRO from ant-design #1
    - https://ng.ant.design
    - the font seems to be pretty small, also can't change the color

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgZorroAntdModule } from 'ng-zorro-antd';
+
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
@@ -12,6 +14,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    NgZorroAntdModule.forRoot(),
     AppRoutingModule
   ],
   providers: [],
