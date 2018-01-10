cd2baa94758507566f7eee2557a4650ea6668c63
at15 <at15@outlook.com>
2018-01-09 17:11:20 -0800

    [agent] Improve script for copy local go repos #4
    - we might need to copy more than one repo, both gommon and go.ice
    might be needed, and go.ice, benchhub also need this script
    - it won't work when GOPATH has multiple paths

--- agent/script/sync_gommon.py
+++
@@ -1,6 +0,0 @@
-from shutil import copytree, ignore_patterns
-
-# hard coded path to sync local gommon to vendor folder https://github.com/benchhub/benchboard/issues/4
-copytree('/home/at15/workspace/src/github.com/dyweb/gommon',
-         '/home/at15/workspace/src/github.com/benchhub/benchboard/agent/vendor/github.com/dyweb/gommon',
-         ignore=ignore_patterns('.git', 'vendor', '.idea'))
